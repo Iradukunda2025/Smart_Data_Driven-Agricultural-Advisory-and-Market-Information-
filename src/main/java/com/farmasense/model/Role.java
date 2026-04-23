6bd1399a31df2a38248c3956971f255635f6901a
Iradukunda <iprc2025@gmail.com>
2026-04-23 09:15:19 +0200

    Final fix: Escape reserved table names and cleanup services

--- src/main/java/com/farmasense/model/Role.java
+++ src/main/java/com/farmasense/model/Role.java
@@ -3,7 +3,7 @@ package com.farmasense.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "`roles`")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
